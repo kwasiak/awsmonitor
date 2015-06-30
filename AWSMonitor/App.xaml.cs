cf39d06a4e21fdcbb51cce4bd3a0408b066ddb77
Chris <k.wasiak@gmail.com>
2015-06-30 19:46:23 +0100

    Adding checks if key secret is defined in the configuration file

--- AWSMonitor/App.xaml.cs
+++ AWSMonitor/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Windows;
 using AwsGateway.Settings;
 
@@ -11,8 +12,26 @@ namespace AWSMonitor
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+
             AwsKeyProviders.Key = ConfigurationManager.AppSettings.Get("Key");
             AwsKeyProviders.Secret = ConfigurationManager.AppSettings.Get("Secret");
+
+            if (string.IsNullOrWhiteSpace(AwsKeyProviders.Key) || string.IsNullOrWhiteSpace(AwsKeyProviders.Key))
+            {
+                MessageBox.Show(
+                    "Please set both: AWS Key and Secret in AWSMonitor.exe.config\n" +
+                    "Application won't work without those settings.\n" + 
+                    "Exiting application now.",
+                    "Configuration error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                Shutdown(1);
+                return;
+            }
+            else
+            {
+                this.StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+            }
         }
     }
 }
